d15f515599294f990d6d392d2619ce13cc1133e9
chrmen <christian.menne@ciber.com>
2014-08-14 14:46:01 +0200

    + added new SQL init script; creates 2 users

---
+++ DB/mysql/create_users.sql
@@ -0,0 +1,27 @@
+	INSERT INTO `comics`.`address` (`idAddress`, `street`, `houseNumber`, `postalCode`, `town`, `careOf`, `company`)
+	VALUES ('1', 'Adminstr.', '1337', '31337', 'Adminstadt', '-', 'Admins Inc.');
+
+	INSERT INTO `comics`.`contactdetails` (`idContactDetails`, `telephoneNumber`, `mobileNumber`, `fax`, `email`, `skypeName`)
+	VALUES ('1', '12345', '12345', '12345', 'admin@admins.com', 'admin_a');
+
+	INSERT INTO `comics`.`registrationdetails` (`idRegistrationDetails`, `ip`, `date`)
+	VALUES ('1', '127.0.0.1', '1');
+
+	INSERT INTO `comics`.`address` (`idAddress`, `street`, `houseNumber`, `postalCode`, `town`, `careOf`, `company`)
+	VALUES ('2', 'Userstr.', '1337', '31337', 'Userstadt', '-', 'Users Inc.');
+
+	INSERT INTO `comics`.`contactdetails` (`idContactDetails`, `telephoneNumber`, `mobileNumber`, `fax`, `email`, `skypeName`)
+	VALUES ('2', '12345', '12345', '12345', 'user@users.com', 'user_u');
+
+	INSERT INTO `comics`.`registrationdetails` (`idRegistrationDetails`, `ip`, `date`)
+	VALUES ('2', '127.0.0.1', '1');
+	
+	commit;
+	
+	INSERT INTO `comics`.`user` (`idUser`, `Address_idAddress`, `RegistrationDetails_idRegistrationDetails`, `ContactDetails_idContactDetails`, `firstname`, `lastname`, `title`, `password`, `role`, `username`, `status`)
+	VALUES ('1', '1', '1', '1', 'Admin', 'Adminsson', 'Prof.', 'admin', 'ROLE_ADMIN', 'admin', 'ACTIVE');
+
+	INSERT INTO `comics`.`user` (`idUser`, `Address_idAddress`, `RegistrationDetails_idRegistrationDetails`, `ContactDetails_idContactDetails`, `firstname`, `lastname`, `title`, `password`, `role`, `username`, `status`) 
+	VALUES ('2', '2', '2', '2', 'User', 'Usersson', 'Dr.', 'user', 'ROLE_USER', 'user', 'ACTIVE');
+
+	commit;

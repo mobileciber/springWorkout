88d285bc3cb93e3d065fd3508c41451fc38f4c37
chrmen <christian.menne@ciber.com>
2014-08-21 13:45:01 +0200

    * now there is a default avatar icon for users who don't have one in the DB

--- code/src/main/java/de/ciber/comics/view/controller/AvatarImageController.java
+++ code/src/main/java/de/ciber/comics/view/controller/AvatarImageController.java
@@ -1,9 +1,22 @@
 package de.ciber.comics.view.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpUtils;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,11 +33,25 @@ public class AvatarImageController {
 	UserRepository repository;
 
 	@RequestMapping("/getImage/{id}")
-	public void getImage(HttpServletResponse response,
-			@PathVariable("id") final long id) throws IOException {
+	public void getImage(HttpServletRequest request,
+			HttpServletResponse response, @PathVariable("id") final long id)
+			throws IOException {
 		response.setContentType("image/jpeg");
 
 		byte[] imageBytes = repository.findOne(id).getAvatarImage();
+
+		if (imageBytes == null || imageBytes.length <= 0) {
+			// FIXME This is horribly ugly; find a better solution
+			//       to get the raw bytes from the default image!
+			
+			URL url = new URL(HttpUtils.getRequestURL(request).toString());
+			URL newUrl = new URL("http://" + url.getHost()
+					+ ":8080" + request.getContextPath()
+					+ "/images/login_icon.jpg");
+			InputStream is = newUrl.openStream();
+			
+			imageBytes = IOUtils.toByteArray(is);
+		}
 		response.getOutputStream().write(imageBytes);
 		response.getOutputStream().flush();
 	}
